5606f7e8c7bf567c91e5a087ec18260a2cc297a1
adumanda <adumanda@stevens.edu>
2016-05-10 10:45:25 -0400

    Update gui.h

--- gui.h
+++ gui.h
@@ -9,3 +9,26 @@
 #include <QtCore>
 #include <QWidget>
 
+namespace Ui {
+class RobotGui;
+}
+
+class RobotGui : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit RobotGui(QWidget *parent = 0);
+    ~RobotGui();
+
+private:
+    Ui::RobotGui *ui;
+protected:
+    void paintEvent(QPaintEvent *);
+private slots:
+    void on_pushButton_clicked();
+    void on_pushButton_2_clicked();
+};
+
+#endif // ROBOTGUI_H
+
