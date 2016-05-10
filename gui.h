3d11c62dc70345f6684ce67d82fa2ccd055b90df
adumanda <adumanda@stevens.edu>
2016-05-10 10:44:23 -0400

    Update and rename robotgui.h to gui.h

---
+++ gui.h
@@ -0,0 +1,10 @@
+//***********************************************************************************
+//Discription: this head file define the basic widget of GUI and a paint event.
+//***********************************************************************************
+
+#include "datalayer.hh"
+#include <QDialog>
+#include <QtGui>
+#include <QtCore>
+#include <QWidget>
+
