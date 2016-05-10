887c27a5e89c3d6f5671eae1d8f8a0f417bdc31f
astro813 <ybao1@stevens.edu>
2016-05-10 10:47:21 -0400

    Update gui.cpp

--- gui.cpp
+++ gui.cpp
@@ -4,12 +4,6 @@
 #include "ui_robotgui.h"
 #include <QtWidgets>
 
-extern Map myMap;
-extern vector<Robot *> robotList;
-
-extern Datalayer myLayer;
-extern vector<Robot *> robotList;
-
 RobotGui::RobotGui(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::RobotGui)
@@ -19,33 +13,33 @@ RobotGui::RobotGui(QWidget *parent) :
     connect(timer, SIGNAL(timeout()),this,SLOT(update()));                             //if timeout then run paintevent();
     timer->start(100);                                                                 //set time as 100ms
 
-    setWindowTitle(tr("Robot Simulator"));                                             //define the title of the window
-    QSize mySize(myLayer.getLength()+200,myLayer.getWidth()+100);                      //define the size of the window
+    setWindowTitle(tr("Robot Simulator (By Yingsheng Bao, Jun Chen, Jiyu Lei, Allison Dumandan)"));                                             //define the title of the window
+    QSize mySize(myMap.getLength()+200,myMap.getWidth()+100);                      //define the size of the window
     this->resize(mySize);
-    ui->pushButton->setGeometry(myLayer.getLength()+15,430,100,20); //add new robot
-    ui->pushButton_2->setGeometry(myLayer.getLength()+15,300,100,20);  //show status
-    ui->label->setGeometry(20,myLayer.getWidth()+15,90,20); 
-    ui->label1->setGeometry(20,myLayer.getWidth()+35,90,20);//velocity
-    ui->label2->setGeometry(20,myLayer.getWidth()+55,90,20);//x
-    ui->label3->setGeometry(20,myLayer.getWidth()+75,90,20);//y
-    ui->label4->setGeometry(200,myLayer.getWidth()+15,90,20);//theta
-    ui->label5->setGeometry(200,myLayer.getWidth()+35,90,20);//radius
-    ui->label6->setGeometry(200,myLayer.getWidth()+55,110,20);//alert range
-    ui->lineEdit->setGeometry(100,myLayer.getWidth()+15,70,20);
-    ui->lineEdit_1->setGeometry(100,myLayer.getWidth()+35,70,20);
-    ui->lineEdit_2->setGeometry(100,myLayer.getWidth()+55,70,20);//robot
-    ui->lineEdit_3->setGeometry(100,myLayer.getWidth()+75,70,20);//location
-    ui->lineEdit_4->setGeometry(280,myLayer.getWidth()+15,70,20);
-    ui->lineEdit_5->setGeometry(280,myLayer.getWidth()+35,70,20);//speed
-    ui->lineEdit_6->setGeometry(300,myLayer.getWidth()+55,70,20);
-    ui->label_2->setGeometry(myLayer.getLength()+20,50,80,20);
-    ui->lineEdit_7->setGeometry(myLayer.getLength()+20,80,50,20);//angle
-    ui->label_3->setGeometry(myLayer.getLength()+20,110,80,20);
-    ui->label_4->setGeometry(myLayer.getLength()+20,140,120,20);
-    ui->label_5->setGeometry(myLayer.getLength()+20,170,65,20);
-    ui->label_6->setGeometry(myLayer.getLength()+20,200,70,20);
-    ui->label_7->setGeometry(myLayer.getLength()+20,230,65,22);
-    ui->label_8->setGeometry(myLayer.getLength()+20,275,70,20);
+    ui->pushButton->setGeometry(myMap.getLength()+15,430,100,20); //add new robot
+    ui->pushButton_2->setGeometry(myMap.getLength()+15,300,100,20); //layout widgets //show status
+    ui->label->setGeometry(20,myMap.getWidth()+15,90,20);
+    ui->label1->setGeometry(20,myMap.getWidth()+35,90,20);
+    ui->label2->setGeometry(20,myMap.getWidth()+55,90,20);
+    ui->label3->setGeometry(20,myMap.getWidth()+75,90,20);
+    ui->label4->setGeometry(200,myMap.getWidth()+15,90,20);
+    ui->label5->setGeometry(200,myMap.getWidth()+35,90,20);
+    ui->label6->setGeometry(200,myMap.getWidth()+55,110,20);
+    ui->lineEdit->setGeometry(100,myMap.getWidth()+15,70,20);
+    ui->lineEdit_1->setGeometry(100,myMap.getWidth()+35,70,20);
+    ui->lineEdit_2->setGeometry(100,myMap.getWidth()+55,70,20);
+    ui->lineEdit_3->setGeometry(100,myMap.getWidth()+75,70,20);
+    ui->lineEdit_4->setGeometry(280,myMap.getWidth()+15,70,20);
+    ui->lineEdit_5->setGeometry(280,myMap.getWidth()+35,70,20);
+    ui->lineEdit_6->setGeometry(300,myMap.getWidth()+55,70,20);
+    ui->label_2->setGeometry(myMap.getLength()+20,50,80,20);
+    ui->lineEdit_7->setGeometry(myMap.getLength()+20,80,50,20);
+    ui->label_3->setGeometry(myMap.getLength()+20,110,80,20);
+    ui->label_4->setGeometry(myMap.getLength()+20,140,120,20);
+    ui->label_5->setGeometry(myMap.getLength()+20,170,65,20);
+    ui->label_6->setGeometry(myMap.getLength()+20,200,70,20);
+    ui->label_7->setGeometry(myMap.getLength()+20,230,65,22);
+    ui->label_8->setGeometry(myMap.getLength()+20,275,70,20);
 }
 
 RobotGui::~RobotGui()
@@ -57,16 +51,16 @@ void RobotGui::paintEvent(QPaintEvent *e){                                      
     QPainter painter(this);
     QPainter painter2(this);
     painter.setRenderHint(QPainter::Antialiasing);                                       //make shape more smooth
-    QColor robotC(127,0,127);
+    QColor robotC(0,0,255);
     QColor boundC(127,127,0);
     QColor AlertC(0,127,127);
-    myLayer.loadData();                                                                  //load data to datalayer and check collision;
-    myLayer.clearData();                                                                 //clear datalayer;
+    myMap.avoidCrash();                                                                  //load data to datalayer and check collision;
+    myMap.clearData();                                                                 //clear datalayer;
     painter.setPen(boundC);
-    painter.drawLine(0,0,myLayer.getLength(),0);
-    painter.drawLine(0,0,0,myLayer.getWidth());
-    painter.drawLine(myLayer.getLength(),0,myLayer.getLength(),myLayer.getWidth());
-    painter.drawLine(0,myLayer.getWidth(),myLayer.getLength(),myLayer.getWidth());
+    painter.drawLine(0,0,myMap.getLength(),0);
+    painter.drawLine(0,0,0,myMap.getWidth());
+    painter.drawLine(myMap.getLength(),0,myMap.getLength(),myMap.getWidth());
+    painter.drawLine(0,myMap.getWidth(),myMap.getLength(),myMap.getWidth());
     for(int i=0; i<robotList.size();i++){
         QPoint myCenter(robotList[i]->getX(),robotList[i]->getY());
         painter2.setPen(AlertC);
@@ -76,7 +70,7 @@ void RobotGui::paintEvent(QPaintEvent *e){                                      
         painter.setBrush(robotC);
         painter.save();
         painter.drawEllipse(myCenter,robotList[i]->getRadius(),robotList[i]->getRadius());
-        robotList[i]->go();                                                               //robot move;
+        robotList[i]->update();                                                               //robot move;
     }
 
 }
@@ -110,7 +104,7 @@ void RobotGui::on_pushButton_2_clicked()
     if(id>=robotList.size()) return;
     QString location='('+QString::number(robotList[id]->getX(),'g',6)+','+QString::number(robotList[id]->getY(),'g',6)+')';
     ui->label_4->setText(location);
-    QString speed=QString::number(robotList[id]->getSpeed(),'g',6);
+    QString speed=QString::number(robotList[id]->getVelocity(),'g',6);
     ui->label_6->setText(speed);
     QString degree=QString::number(robotList[id]->getTheta(),'g',6);
     ui->label_8->setText(degree);
